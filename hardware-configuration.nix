9b4139d4250a3a50f6a21059e2e55260a9d8facf
Svend Sorensen <svend@svends.net>
2020-05-02 14:48:02 -0700

    nixos-generate-config

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,7 +8,8 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
