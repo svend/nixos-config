41a8ea5df3c6e2a14ce9e59f1172cb98120e15c0
Svend Sorensen <svend@svends.net>
2020-09-27 10:16:02 -0700

    Yay!

--- overlays/dual-function-keys.nix
+++ overlays/dual-function-keys.nix
@@ -1,8 +1,6 @@
 self: super:
 {
-  # TODO: find way to add to existing interception-tools-plugins
-  interception-tools-plugins = {
-    caps2esc = super.interception-tools-plugins.caps2esc;
+  interception-tools-plugins = super.interception-tools-plugins // {
     dual-function-keys = super.callPackage ../pkgs/dual-function-keys.nix { };
   };
 }
