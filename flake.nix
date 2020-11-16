dd65875a076a5743aad93b555824470a953a12b4
Svend Sorensen <svend@svends.net>
2020-11-15 17:30:34 -0800

    Update

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+
   outputs = { self, nixpkgs }: {
       nixosConfigurations.quartz = nixpkgs.lib.nixosSystem {
        system = "x86_64-linux";
