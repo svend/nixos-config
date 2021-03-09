4232f8ae1a799f63b2091dde36b9f0aa64ea977f
Svend Sorensen <svend@svends.net>
2021-03-08 20:32:11 -0800

    Update

--- wireguard.nix
+++ wireguard.nix
@@ -1,48 +1,21 @@
 { config, pkgs, ... }:
 {
-  networking.wireguard.enable = true;
-
-  networking.firewall = {
-    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
-  };
-
-  #  boot.extraModulePackages = with config.boot.kernelPackages; [ wireguard ];
-
   environment.systemPackages = with pkgs; [
     qrencode # to print WireGuard QR codes
   ];
 
-  networking.wireguard.interfaces = {
+  networking.wg-quick.interfaces = {
     wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.0.0.2/24" "fd00::2/48" ];
-      listenPort = 51820;
-
-      # Default IPv6 route has lower precedence
-      # default via fe80::d6ca:6dff:fe06:c6b1 dev wlp3s0 proto ra metric 600 pref medium
-      # default dev wg0 metric 1024 pref medium
-
-      # postSetup = "ip route replace ::/0 dev wg0 metric 50 table main";
-      # postShutdown = "ip route delete ::/0 dev wg0 metric 50 table main";
-
-      privateKeyFile = "/etc/nixos-secrets/wireguard/private";
-
+      address = [ "10.0.0.2/24" "fd00::2/64" ];
+      dns = [ "10.0.0.9" ];
       peers = [
         {
-          publicKey = "VfEKXJmJ+moe4DfTv8bDJlHiwQKc4KYaOqzeIacJxG4=";
-          allowedIPs = [ "10.0.0.9/32" "fd00::9/64" ];
-          # allowedIPs = [ "0.0.0.0/0" "::/0" ];
+          allowedIPs = [ "0.0.0.0/0" "::/0" ];
           endpoint = "c0.us-west.svends.net:51820";
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
+          publicKey = "VfEKXJmJ+moe4DfTv8bDJlHiwQKc4KYaOqzeIacJxG4=";
         }
-        # {
-        #   publicKey = "XX0kFECvZZpdvvCU0pGsJJS2woqsw+puP403tQcX0gc=";
-        #   allowedIPs = [ "10.0.0.8/32" "fd00::8/64" ];
-        #   endpoint = "wg1.i.svends.net:51820";
-        # }
       ];
+      privateKeyFile = "/etc/nixos-secrets/wireguard/private";
     };
   };
 }
