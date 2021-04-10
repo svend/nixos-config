c3462af07c9340585a0bf7bc75e8ab534df6c4ae
Svend Sorensen <svend@svends.net>
2021-04-10 11:05:23 -0700

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
 
   outputs = { self, nixos, nixos-unstable, nixpkgs-unstable }: {
-    nixosConfigurations.quartz = nixos-unstable.lib.nixosSystem {
+    nixosConfigurations.quartz = nixos.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./quartz.nix ];
     };
