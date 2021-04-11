ebf11aa8a0082e45e92117fd36d0b49b96e5040e
Svend Sorensen <svend@svends.net>
2021-04-10 20:52:55 -0700

    Disable gnome-keyring
    This overrides SSH_AUTH_SOCK from gpg-agent.

--- xserver.nix
+++ xserver.nix
@@ -14,6 +14,9 @@
   # Enable the Gnome Desktop Environment
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome3.enable = true;
+  # I use gpg-agent for SSH
+  # https://github.com/NixOS/nixpkgs/issues/42291#issuecomment-687979733
+  services.gnome3.gnome-keyring.enable = pkgs.lib.mkForce false;
 
   environment.systemPackages = with pkgs; [
     chromium
