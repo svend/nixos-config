bc158f53cb31453c844264b42e08cbb8a926c8b2
Svend Sorensen <svend@svends.net>
2021-09-30 13:26:47 -0700

    Use local wireguard server

--- wireguard.nix
+++ wireguard.nix
@@ -7,12 +7,15 @@
   networking.wg-quick.interfaces = {
     wg0 = {
       address = [ "10.0.0.2/24" "fd00::2/64" ];
-      dns = [ "10.0.0.9" ];
+      dns = [ "1.1.1.1" ];
+      # dns = [ "10.0.0.9" ];
       peers = [
         {
           allowedIPs = [ "0.0.0.0/0" "::/0" ];
-          endpoint = "c0.us-west.svends.net:51820";
-          publicKey = "VfEKXJmJ+moe4DfTv8bDJlHiwQKc4KYaOqzeIacJxG4=";
+          endpoint = "192.168.0.21:51820";
+          publicKey = "Kd1JuRVmitgnwVfesMFB5z7E4saFoWSiZPzVuiafn1w=";
+          # endpoint = "c0.us-west.svends.net:51820";
+          # publicKey = "VfEKXJmJ+moe4DfTv8bDJlHiwQKc4KYaOqzeIacJxG4=";
         }
       ];
       privateKeyFile = "/etc/nixos-secrets/wireguard/private";
