9daf4fab36d6699c63a8b433b28016a039f84dc6
Svend Sorensen <svend@svends.net>
2021-11-30 15:56:28 -0800

    Switch to nixos 21.11

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
   inputs = {
     nixos.url = "nixpkgs/release-21.05";
+    nixos-next.url = "nixpkgs/release-21.11";
     nixos-unstable.url = "nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
   };
 
 
-  outputs = { self, nixos, nixos-unstable, nixpkgs-unstable }: {
-    nixosConfigurations.quartz = nixos-unstable.lib.nixosSystem {
+  outputs = { self, nixos, nixos-next, nixos-unstable, nixpkgs-unstable }: {
+    nixosConfigurations.quartz = nixos-next.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./quartz.nix ];
     };
