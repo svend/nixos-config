898c226198bc8bbd4f996094ee644c586757d91f
Svend Sorensen <svend@svends.net>
2022-12-01 09:39:36 -0800

    Update comments

--- wireguard.nix
+++ wireguard.nix
@@ -5,10 +5,10 @@
   ];
 
   networking.wg-quick.interfaces = {
-    # Raspberry Pi
     wg0 = {
       address = [ "10.0.0.2/24" "fd00::2/64" ];
       peers = [
+        # Raspberry Pi
         {
           # allowedIPs = [ "0.0.0.0/0" "::/0" ];
           allowedIPs = [ "10.0.0.0/24" "fd00::/64" ];
@@ -16,6 +16,7 @@
           endpoint = "10.10.0.238:51820";
           publicKey = "Kd1JuRVmitgnwVfesMFB5z7E4saFoWSiZPzVuiafn1w=";
         }
+        # Digital Ocean
         {
           allowedIPs = [ "10.0.0.9/32" "fd00::9/128" ];
           endpoint = "c0.us-west.svends.net:51820";
@@ -24,23 +25,5 @@
       ];
       privateKeyFile = "/etc/nixos-secrets/wireguard/private";
     };
-
-    # DigitalOcean
-    # wg0 = {
-    #   address = [ "10.0.0.2/24" "fd00::2/64" ];
-    #   # dns = [ "10.0.0.9" ];
-    #   peers = [
-    #     {
-    #       allowedIPs = [ "0.0.0.0/0" "::/0" ];
-    #       endpoint = "c0.us-west.svends.net:51820";
-    #       publicKey = "VfEKXJmJ+moe4DfTv8bDJlHiwQKc4KYaOqzeIacJxG4=";
-    #     }
-    #     {
-    #       allowedIPs = [ "10.0.0.5/32" ];
-    #       publicKey = "Kd1JuRVmitgnwVfesMFB5z7E4saFoWSiZPzVuiafn1w=";
-    #     }
-    #   ];
-    #   privateKeyFile = "/etc/nixos-secrets/wireguard/private";
-    # };
   };
 }
