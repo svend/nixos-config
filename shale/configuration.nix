ed7810980928a2e1bfd1504e23f33f21018bf2eb
Svend Sorensen <svend@svends.net>
2023-01-17 22:27:23 -0800

    Enable gnupg

--- shale/configuration.nix
+++ shale/configuration.nix
@@ -122,6 +122,12 @@
   #   enableSSHSupport = true;
   # };
 
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
