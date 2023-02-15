df2cd0d3e9ff4c0f1c07e0bfbea9055aaf72e081
Svend Sorensen <svend@svends.net>
2023-02-15 14:01:29 -0800

    Remove wireguard endpoint

--- wireguard.nix
+++ wireguard.nix
@@ -16,12 +16,12 @@
           endpoint = "10.10.0.238:51820";
           publicKey = "Kd1JuRVmitgnwVfesMFB5z7E4saFoWSiZPzVuiafn1w=";
         }
-        # Digital Ocean
-        {
-          allowedIPs = [ "10.0.0.9/32" "fd00::9/128" ];
-          endpoint = "c0.us-west.svends.net:51820";
-          publicKey = "VfEKXJmJ+moe4DfTv8bDJlHiwQKc4KYaOqzeIacJxG4=";
-        }
+        # # Digital Ocean
+        # {
+        #   allowedIPs = [ "10.0.0.9/32" "fd00::9/128" ];
+        #   endpoint = "c0.us-west.svends.net:51820";
+        #   publicKey = "VfEKXJmJ+moe4DfTv8bDJlHiwQKc4KYaOqzeIacJxG4=";
+        # }
       ];
       # sudo mkdir -p -m 700 /etc/nixos-secrets/wireguard
       # wg genkey | sudo tee /etc/nixos-secrets/wireguard/private >/dev/null
